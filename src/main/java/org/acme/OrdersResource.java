683cc5ca4f9fe03f60e7927b6f76f90f1718d419
Thomas Bertz <tom@thomas-a-bertz.de>
2026-01-21 02:29:08 +0100

    führe temporäre Map als flüchtigen Datenspeicher ein

--- src/main/java/org/acme/OrdersResource.java
+++ src/main/java/org/acme/OrdersResource.java
@@ -4,24 +4,34 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Path("/orders")
 public class OrdersResource {
 
-    private final Order dummyOrder;
+    private final Map<UUID, Order> orders =  new HashMap<>();
+
     {
-        dummyOrder = new Order();
+        Order dummyOrder = new Order();
         dummyOrder.setOrderId(UUID.randomUUID());
         dummyOrder.setCustomerLastname("Doe");
         dummyOrder.setCustomerFirstname("John");
         dummyOrder.setItemDescription("Apple");
         dummyOrder.setAmount(5);
+        orders.put(dummyOrder.getOrderId(), dummyOrder);
+        dummyOrder = new Order();
+        dummyOrder.setOrderId(UUID.randomUUID());
+        dummyOrder.setCustomerLastname("Russel");
+        dummyOrder.setCustomerFirstname("Jack");
+        dummyOrder.setItemDescription("Banana");
+        dummyOrder.setAmount(3);
+        orders.put(dummyOrder.getOrderId(), dummyOrder);
     }
 
     @GET
     public Response listOrders() {
-        return Response.ok(List.of(dummyOrder)).build();
+        return Response.ok(orders.values()).build();
     }
 }
