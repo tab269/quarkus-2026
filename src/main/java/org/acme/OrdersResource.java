2b405e6fc0ab464382c1cd62ed207b6e98abece0
Thomas Bertz <tom@thomas-a-bertz.de>
2026-01-21 03:23:28 +0100

    räume auf
    - entferne Dummy-Werte in der Orders-Map
    - entferne GreetingResource
    - entferne Tests für GreetingResource

--- src/main/java/org/acme/OrdersResource.java
+++ src/main/java/org/acme/OrdersResource.java
@@ -19,23 +19,6 @@ public class OrdersResource {
 
     private final Map<UUID, Order> orders =  new HashMap<>();
 
-    {
-        Order dummyOrder = new Order();
-        dummyOrder.setOrderId(UUID.randomUUID());
-        dummyOrder.setCustomerLastname("Doe");
-        dummyOrder.setCustomerFirstname("John");
-        dummyOrder.setItemDescription("Apple");
-        dummyOrder.setAmount(5);
-        orders.put(dummyOrder.getOrderId(), dummyOrder);
-        dummyOrder = new Order();
-        dummyOrder.setOrderId(UUID.randomUUID());
-        dummyOrder.setCustomerLastname("Russel");
-        dummyOrder.setCustomerFirstname("Jack");
-        dummyOrder.setItemDescription("Banana");
-        dummyOrder.setAmount(3);
-        orders.put(dummyOrder.getOrderId(), dummyOrder);
-    }
-
     @GET
     public Response listOrders() {
         return Response.ok(orders.values()).build();
