d18ede8ba1563bdf4c6285c54d8ca015e6ec0581
Thomas Bertz <tom@thomas-a-bertz.de>
2026-01-21 01:57:20 +0100

    gib leere Liste zurück

--- src/main/java/org/acme/OrdersResource.java
+++ src/main/java/org/acme/OrdersResource.java
@@ -4,11 +4,13 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 
+import java.util.List;
+
 @Path("/orders")
 public class OrdersResource {
 
     @GET
     public Response listOrders() {
-        return Response.ok().build();
+        return Response.ok(List.of()).build();
     }
 }
