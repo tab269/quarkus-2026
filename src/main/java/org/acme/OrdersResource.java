7fd6b42b25c0c6d477605975096bc8d3ef15bd93
Thomas Bertz <tom@thomas-a-bertz.de>
2026-01-21 02:21:38 +0100

    implementiere Order-Klasse nach RQ1 und erweitere leere Beispiel-Instanz

--- src/main/java/org/acme/OrdersResource.java
+++ src/main/java/org/acme/OrdersResource.java
@@ -5,12 +5,23 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.core.Response;
 
 import java.util.List;
+import java.util.UUID;
 
 @Path("/orders")
 public class OrdersResource {
 
+    private final Order dummyOrder;
+    {
+        dummyOrder = new Order();
+        dummyOrder.setOrderId(UUID.randomUUID());
+        dummyOrder.setCustomerLastname("Doe");
+        dummyOrder.setCustomerFirstname("John");
+        dummyOrder.setItemDescription("Apple");
+        dummyOrder.setAmount(5);
+    }
+
     @GET
     public Response listOrders() {
-        return Response.ok(List.of(new Order())).build();
+        return Response.ok(List.of(dummyOrder)).build();
     }
 }
