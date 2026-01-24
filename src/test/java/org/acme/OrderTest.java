55267cfe77878e6c9c62b91dedc05973cfc3b8c1
Thomas Bertz <tom@thomas-a-bertz.de>
2026-01-24 23:26:31 +0100

    refactor and add more tests

--- src/test/java/org/acme/OrderTest.java
+++ src/test/java/org/acme/OrderTest.java
@@ -10,6 +10,10 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -17,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class OrderTest {
 
+    public static final String FOURTY_ONE_CHARS = "12345678901234567890123456789012345678901";
     private static Validator validator;
     private static ValidatorFactory validatorFactory;
 
@@ -32,33 +37,57 @@ class OrderTest {
     }
 
     @ParameterizedTest
-    @MethodSource("customerLastnameInputValueAndExpectedViolationPairsProvider")
-    void validateCustomerLastnameOnOrder(String customerLastnameInput, String expectedViolationMessage) {
-        var order = new Order();
-        order.setCustomerLastname(customerLastnameInput);
+    @MethodSource("createOrdersTestSpecification")
+    <T> void validateCustomerLastnameOnOrder(String fieldName, T fieldValue, String expectedViolationMessage)
+            throws IntrospectionException, InvocationTargetException, IllegalAccessException {
+        Order testee = createTestOrder(fieldName, fieldValue);
 
-        Set<ConstraintViolation<Order>> violations = validator.validate(order);
+        Set<ConstraintViolation<Order>> violations = validator.validate(testee);
 
         if (expectedViolationMessage == null) {
             assertEquals(0, violations.size()); // valid = happy path
         } else {
             long count = violations.stream()
-                    .filter(v -> v.getPropertyPath().toString().equals("customerLastname"))
+                    .filter(v -> v.getPropertyPath().toString().equals(fieldName))
                     .filter(v -> v.getMessage().equals(expectedViolationMessage))
                     .count();
             assertEquals(1, count); // invalid = violation path
         }
     }
 
-    static Stream<Arguments> customerLastnameInputValueAndExpectedViolationPairsProvider() {
+    private <T> Order createTestOrder(String fieldName, T fieldValue)
+            throws IntrospectionException, InvocationTargetException, IllegalAccessException {
+        var order = new Order();
+        // initialize a valid object
+        order.setCustomerLastname("valid from Test");
+        order.setCustomerFirstname("valid from Test");
+        order.setItemDescription("valid from Test");
+        order.setAmount(10);
+
+        // and override the field for the test at hand
+        var propertyDescriptor = new PropertyDescriptor(fieldName, Order.class);
+        Method setter = propertyDescriptor.getWriteMethod();
+        setter.invoke(order, fieldValue);
+
+        return order;
+    }
+
+    static Stream<Arguments> createOrdersTestSpecification() {
         return Stream.of(
-                // input value, expected violation message
-                Arguments.of(null, "must not be null"),
-                Arguments.of("", "must not be empty"),
-                Arguments.of("     ", "must not be blank"),
-                Arguments.of("A", "size must be between 2 and 40"),
-                Arguments.of("12345678901234567890123456789012345678901", "size must be between 2 and 40"),
-                Arguments.of("Maier", null) // valid
+                // fieldName, fieldValue, expectedViolationMessage
+                Arguments.of("customerLastname", null, "must not be null"),
+                Arguments.of("customerLastname", "", "must not be empty"),
+                Arguments.of("customerLastname", "     ", "must not be blank"),
+                Arguments.of("customerLastname", "A", "size must be between 2 and 40"),
+                Arguments.of("customerLastname", FOURTY_ONE_CHARS, "size must be between 2 and 40"),
+                Arguments.of("customerLastname", "Maier", null), // valid
+
+                Arguments.of("customerFirstname", null, null), // valid
+                Arguments.of("customerFirstname", "", null), // valid
+                Arguments.of("customerFirstname", "     ", null), // valid
+                Arguments.of("customerFirstname", "A", "size must be between 2 and 40"),
+                Arguments.of("customerFirstname", FOURTY_ONE_CHARS, "size must be between 2 and 40"),
+                Arguments.of("customerFirstname", "Max", null) // valid
         );
     }
 }
