3aff3c800dd972f41c098dee1c70df4c447c45a8
Thomas Bertz <tom@thomas-a-bertz.de>
2026-01-28 22:43:21 +0100

    reactive

--- src/main/java/org/acme/boundary/OrderResource.java
+++ src/main/java/org/acme/boundary/OrderResource.java
@@ -1,5 +1,6 @@
 package org.acme.boundary;
 
+import io.smallrye.mutiny.Uni;
 import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.*;
@@ -10,8 +11,10 @@ import org.acme.domain.model.OrderEntity;
 import org.acme.domain.service.OrderService;
 
 import java.net.URI;
-import java.util.List;
 import java.util.UUID;
+
+import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
+import static org.acme.boundary.OrderMapper.toDTO;
 
 @Path("/orders")
 public class OrderResource implements OrderAPI {
@@ -23,71 +26,71 @@ public class OrderResource implements OrderAPI {
         this.orderService = orderService;
     }
 
-    // public Response erzeugeOrder(@Valid OrderDTO orderDTO, @Context UriInfo uriInfo) {
-    public Response erzeugeOrder(@Valid OrderDTO orderDTO) {
-        // Alternative Möglichkeit zur Valid-Annotation am Parameter,
-        // um z.B. Fehlermeldungen zu loggen aber NICHT via REST zu responden (security)
-        // Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
-        // Set<ConstraintViolation<OrderDTO>> validationResult = validator.validate(orderDTO);
-        // validationResult.forEach(violation -> {
-        //     violation.getPropertyPath().forEach(propertyPath -> {
-        //         ...
-        //     });
-        // });
+    public Uni<Response> erzeugeOrder(@Valid OrderDTO orderDTO) {
         if (orderDTO.customerFirstname == null || orderDTO.customerFirstname.isBlank()) {
             orderDTO.customerFirstname = null;
         }
         OrderEntity orderEntity = OrderMapper.toEntity(orderDTO);
-        OrderEntity orderEntityPersisted = orderService.save(orderEntity);
-        OrderDTO orderDTOResult = OrderMapper.toDTO(orderEntityPersisted);
-        URI location = UriBuilder
-                .fromResource(OrderResource.class)
-                .path(orderDTOResult.getOrderId().toString())
-                .build();
+        /* bis hier läuft alles noch im Event-Loop-Thread: ist OK, weil kurz und CPU-only */
 
-        return Response.created(location).build();
+        return orderService.save(orderEntity)
+                .onItem().transform(persistedEntity -> {
+                   var orderDTOResult = toDTO(persistedEntity);
+
+                    URI location = UriBuilder
+                            .fromResource(OrderResource.class)
+                            .path(orderDTOResult.getOrderId().toString())
+                            .build();
+
+
+                   return Response.created(location).build();
+                });
     }
 
     @GET
     @Path("/findByAmountGreaterThan")
-    public Response findOrdersByAmountGreaterThan(@QueryParam("amount") int amount) {
-        List<OrderDTO> foundOrders = orderService.findAll().stream()
-                .filter(orderEntity -> orderEntity.getAmount() > amount)
-                .map(OrderMapper::toDTO)
-                .toList();
-        return Response.ok(foundOrders).build();
+    public Uni<Response> findOrdersByAmountGreaterThan(@QueryParam("amount") int amount) {
+        return orderService.findAll()
+                .onItem().transform(orderEntities ->
+                        orderEntities.stream()
+                                .filter(orderEntity -> orderEntity.getAmount() > amount)
+                                .map(OrderMapper::toDTO)
+                                .toList())
+                .onItem().transform(orderDtos -> Response.ok(orderDtos).build());
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @Path("/filter")
-    public Response filter(OrderFilterDTO orderFilterDTO) {
-        List<OrderDTO> foundOrders = orderService.findAll().stream()
-                .filter(o -> switch (orderFilterDTO.getFilterOperator()) {
-                                        case LESS_THAN -> o.getAmount() < orderFilterDTO.getAmount();
-                                        case GREATER_THAN -> o.getAmount() > orderFilterDTO.getAmount();
-                    })
-                .map(OrderMapper::toDTO)
-                .toList();
-        return Response.ok(foundOrders).build();
+    public Uni<Response> filter(OrderFilterDTO orderFilterDTO) {
+
+        return orderService.findAll()
+                .onItem().transform(orders ->
+                        orders.stream()
+                                .filter(orderEntity -> switch (orderFilterDTO.getFilterOperator()) {
+                                    case LESS_THAN -> orderEntity.getAmount() < orderFilterDTO.getAmount();
+                                    case GREATER_THAN -> orderEntity.getAmount() > orderFilterDTO.getAmount();
+                                }).map(OrderMapper::toDTO)
+                                .toList())
+                .onItem().transform(orderDtos ->
+                        Response.ok(orderDtos).build());
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getOrders() {
-        return Response.ok(orderService.findAll().stream()
-                .map(OrderMapper::toDTO)
-                .toList()).build();
+    public Uni<Response> getOrders() {
+        return orderService.findAll()
+                .onItem()
+                .transform(orderEntities -> Response.ok(orderEntities).build());
     }
 
     @GET
     @Path("/{orderId}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getOrder(@PathParam("orderId") UUID orderId) {
+    public Uni<Response> getOrder(@PathParam("orderId") UUID orderId) {
         return orderService.findById(orderId)
-                .map(entity -> Response.ok(OrderMapper.toDTO(entity)).build())
-                .orElse(Response.status(Response.Status.NOT_FOUND)
-                        .entity("Es existiert keine OrderDTO mit der orderId " + orderId).build());
+                .onItem().ifNotNull().transform(orderEntity -> Response.ok(toDTO(orderEntity)).build())
+                .onItem().ifNull().continueWith(Response.status(NOT_FOUND).build());
     }
 }
--- src/main/java/org/acme/domain/service/OrderService.java
+++ src/main/java/org/acme/domain/service/OrderService.java
@@ -1,35 +1,58 @@
 package org.acme.domain.service;
 
+import io.quarkus.narayana.jta.QuarkusTransaction;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.infrastructure.Infrastructure;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.transaction.Transactional;
 import org.acme.domain.model.OrderEntity;
 import org.acme.persistence.OrdersRepository;
 
-import java.util.*;
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ScheduledExecutorService;
 
 @ApplicationScoped
 public class OrderService {
 
-    private final OrdersRepository orderOrdersRepository;
+    public static final ScheduledExecutorService DEFAULT_WORKER_POOL = Infrastructure.getDefaultWorkerPool();
+    private final OrdersRepository ordersRepo;
 
     @Inject
     public OrderService(OrdersRepository orderRepository) {
-        this.orderOrdersRepository = orderRepository;
+        this.ordersRepo = orderRepository;
     }
 
-    @Transactional
-    public OrderEntity save(OrderEntity orderEntity) {
+    // hier noch klassische Transaktion weil Panache blockierend ist
+    // bei reactive-Panache dann @WithTransaction verwenden!
+    // ABER: @Transactional muss hier weg und **in das Uni rein**!
+    // @Transactional
+    public Uni<OrderEntity> save(OrderEntity orderEntity) {
         orderEntity.setOrderId(UUID.randomUUID());
-        orderOrdersRepository.persist(orderEntity);
-        return orderEntity;
+
+        return Uni.createFrom()
+                .item(() -> {
+                    QuarkusTransaction.begin();
+                    try {
+                        ordersRepo.persist(orderEntity);
+                        QuarkusTransaction.commit();
+                        return orderEntity;
+                    } catch (RuntimeException e) {
+                        QuarkusTransaction.rollback();
+                        throw e;
+                    }
+                }).runSubscriptionOn(DEFAULT_WORKER_POOL);
     }
 
-    public Collection<OrderEntity> findAll() {
-        return orderOrdersRepository.listAll();
+    public Uni<List<OrderEntity>> findAll() {
+        return Uni.createFrom()
+                .item(ordersRepo::listAll)
+                .runSubscriptionOn(DEFAULT_WORKER_POOL);
     }
 
-    public Optional<OrderEntity> findById(UUID orderId) {
-        return orderOrdersRepository.findByOrderId(orderId);
+    public Uni<OrderEntity> findById(UUID orderId) {
+        return Uni.createFrom()
+                .item(() -> ordersRepo.findByOrderId(orderId).orElse(null))
+                .runSubscriptionOn(DEFAULT_WORKER_POOL);
     }
 }
